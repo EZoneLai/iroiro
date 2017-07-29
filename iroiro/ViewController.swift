c0407cd7a9990d3a1678394989a807cd3f4962be
EZoneLai Lai <ezonelai@gmail.com>
2017-07-29 15:20:58 +0800

    Initial Commit

---
+++ iroiro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iroiro
+//
+//  Created by EZoneLai Lai on 2017/7/29.
+//  Copyright © 2017年 EZoneLai Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
